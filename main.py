767130ea68b5371e3b7dc2b3ecf86457237037ec
Chia-Chen Chu <jerry955071@gmail.com>
2024-01-30 16:06:42 +0800

    add connection to public google sheet

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
 st.write("""Hello, world!""")
+conn = st.connection()
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read()
+st.write(df)
